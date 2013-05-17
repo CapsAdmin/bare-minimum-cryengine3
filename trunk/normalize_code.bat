897b7948470b63348ebfea0446ed5b105b178a76
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@3079bb93-4af0-222a-757d-b39309811f0b>
2013-05-17 12:22:33 +0000

    added the untouched CryeEngine3 SDK CryGame code added a portable winmerge with a batch file in root you can run to compare the original code against the minimal code

--- trunk/normalize_code.bat
+++ trunk/normalize_code.bat
@@ -1,2 +1,4 @@
-astyle.exe -A1 -T -C -S -K -N -L -w -Y -f -p -xe -k3 -W3 -j code/CryGame/*.cpp code/CryGame/*.h
+cd tools/astyle/
+astyle.exe -A1 -T -C -S -K -N -L -w -Y -f -p -xe -k3 -W3 -j ../../code/CryGame/*.cpp ../../code/CryGame/*.h
+astyle.exe -A1 -T -C -S -K -N -L -w -Y -f -p -xe -k3 -W3 -j ../../code/CryGame_Original/*.cpp ../../code/CryGame_Original/*.h
 pause
